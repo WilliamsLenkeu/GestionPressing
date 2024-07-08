14d3f952333d45ef46b53f20a3138a5ef66bd09e
Wally🤖 <williams25prince@gmail.com>
2024-07-08 22:35:52 +0100

    ajout de firebase

--- lib/ui/screens/cart_page.dart
+++ lib/ui/screens/cart_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../models/order.dart';
+
 class CartPage extends StatefulWidget {
-  const CartPage({super.key});
+  final String? serviceType;
+
+  const CartPage({super.key, this.serviceType});
 
   @override
   State<CartPage> createState() => _CartPageState();
@@ -11,6 +15,16 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
+    List<Order> filteredOrders;
+
+    if (widget.serviceType == null) {
+      filteredOrders = Order.orderList;
+    } else {
+      filteredOrders = Order.orderList
+          .where((order) => order.serviceType == widget.serviceType)
+          .toList();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -19,6 +33,18 @@ class _CartPageState extends State<CartPage> {
         ),
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: filteredOrders.length,
+        itemBuilder: (context, index) {
+          final order = filteredOrders[index];
+          return ListTile(
+            leading: Image.asset(order.serviceImage),
+            title: Text(order.itemDescription),
+            subtitle: Text('Client: ${order.customerName}\nAdresse: ${order.customerAddress}\nStatut: ${order.status}'),
+            trailing: Text('${order.totalAmount} €'),
+          );
+        },
       ),
     );
   }
